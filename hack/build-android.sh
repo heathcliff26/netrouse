d4095f54abb5972d403bba947a285bd216b8da76
Heathcliff <heathcliff@heathcliff.eu>
2026-09-20 18:03:57 +0200

    android: Build new bundle format
    Additionally fix settings tab layout.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/build-android.sh
+++ hack/build-android.sh
@@ -16,10 +16,39 @@ done
 
 sed -i 's#../../##g' FyneApp.toml
 
-bin/fyne package --os android --release
+if [ -z "${KEYSTORE}" ]; then
+    echo "No keystore specified, using debug keystore"
+    export KEYSTORE="debug.keystore"
+    export KEYSTORE_PASS="android"
+    export KEYSTORE_ALIAS="androiddebugkey"
+    if [ ! -e "${KEYSTORE}" ]; then
+        keytool -genkeypair -v \
+            -keystore "${KEYSTORE}" \
+            -storepass "${KEYSTORE_PASS}" \
+            -alias "${KEYSTORE_ALIAS}" \
+            -keypass android \
+            -keyalg RSA \
+            -keysize 2048 \
+            -validity 10000 \
+            -dname "CN=NetRouse Debug,O=NetRouse,C=DE"
+    fi
+fi
+
+bin/fyne release --os android --app-build 1 \
+    --keystore "${KEYSTORE}" \
+    --keystore-pass "${KEYSTORE_PASS}" \
+    --key-name "${KEYSTORE_ALIAS}"
+
+bin/fyne package --os android --release --app-build 1
+apksigner sign --ks "${KEYSTORE}" --ks-pass "pass:${KEYSTORE_PASS}" --ks-key-alias "${KEYSTORE_ALIAS}" \
+    --v1-signing-enabled true \
+    --v2-signing-enabled true \
+    --v3-signing-enabled true \
+    NetRouse.apk
 
 mkdir -p dist
-mv ./*.apk dist/
+mv NetRouse.aab NetRouse.apk dist/
+rm NetRouse.apk.idsig
 
 for file in "${files[@]}"; do
     rm "${file}"
