2aac1621d08c678f48ede669dae2b416de5f98a7
Heathcliff <heathcliff@heathcliff.eu>
2026-09-21 19:07:54 +0200

    android: Use adaptive Icon
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/common.sh
+++ hack/common.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # renovate: datasource=docker depName=ghcr.io/heathcliff26/go-fyne-ci extractVersion=^(?<version>.*)$
-export BUILDER_VERSION=202609201801
+export BUILDER_VERSION=202609211707
 export BUILDER_IMAGE="${BUILDER_IMAGE:-ghcr.io/heathcliff26/go-fyne-ci:$BUILDER_VERSION}"
